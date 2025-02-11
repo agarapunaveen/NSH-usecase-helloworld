2748f3853418abe776722ea99c164e05cff8f11e
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-11 13:51:42 +0530

    Update variables.tf

--- Infra/Modules/LAMDA/variables.tf
+++ Infra/Modules/LAMDA/variables.tf
@@ -6,7 +6,9 @@ variable "region" {
   default     = "us-east-1"
 }
 
-
+variable "image_name"{
+default="010928202531.dkr.ecr.us-east-1.amazonaws.com/hackathon/helloworld:latest"
+}
 
 variable "lambda_role_arn" {
 }

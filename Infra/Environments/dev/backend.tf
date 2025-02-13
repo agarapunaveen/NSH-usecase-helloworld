4f2fc49919ed96efdca9109559de34dda5176928
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-13 09:52:50 +0530

    Update backend.tf

--- Infra/Environments/dev/backend.tf
+++ Infra/Environments/dev/backend.tf
@@ -1,11 +1,11 @@
- terraform{
+ /*terraform{
    backend "s3" {
      bucket         = "52297389-bucket-1"
      key            = "dev/terraform/EKS/terraform.tfstate"
      region         = "us-east-1"
      encrypt        = true
    }
- }
+ }*/
 
 
 

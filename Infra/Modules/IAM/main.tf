88e93907c01eaa6ecacdba1f63b57005dbbde312
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-13 11:38:40 +0530

    Update main.tf

--- Infra/Modules/IAM/main.tf
+++ Infra/Modules/IAM/main.tf
@@ -21,3 +21,12 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+#OIDC
+/*resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+}*/
